c6815a8d030ef6b5e8bdf583cd79c9347eacdcc0
SteveH498 <stefan.haas1@gmx.de>
2016-05-05 10:27:12 +0200

    Wait for internet connection
    Wait for internet connection to determine proxy or non-proxy environment

--- working_hours_tracker.py
+++ working_hours_tracker.py
@@ -1,4 +1,4 @@
-import sys, argparse, sqlite3, requests
+import sys, argparse, sqlite3, requests, time
 
 def _insert_timestamp(args):
     ''' Insert either startup or shutdown time stamp into sqlite database working_hours.db '''    
@@ -18,7 +18,10 @@ def _insert_timestamp(args):
     else:
         raise ValueError('--startup or --shutdown flag must be set')
     
-    proxy = check_if_proxy_available(http_proxy,https_proxy)
+    proxy = check_connection(http_proxy,https_proxy)
+        
+    if not proxy:
+        return
                 
     # Insert time stamp into sqlite db
     conn = sqlite3.connect("working_hours.db")
@@ -31,16 +34,35 @@ def _insert_timestamp(args):
     conn.close()
 
 
-def check_if_proxy_available(http_proxy,https_proxy):
+def check_connection(http_proxy,https_proxy):
     proxies = {
         "http": http_proxy,
         "https": https_proxy,
     }    
-    proxy = proxies["http"]
-    try:
-        requests.get('http://www.google.com', proxies=proxies,timeout=0.001)      
-    except:
-        proxy = ""    
+    proxy = ""
+    
+    # Wait until internet connection is available
+    start = time.time()
+    while True:
+        # Check for non-proxy environment
+        try:
+            requests.get("http://www.google.com")
+            proxy = "None"
+            break
+        except:
+            pass
+        # Check for proxy environment
+        try:
+            requests.get("http://www.google.com", proxies=proxies)    
+            proxy = proxies["http"]  
+            break       
+        except:
+            pass
+        
+        end = time.time() - start
+        if end > 300: #Timeout after 5 minutes
+            print("Timeout")
+            break       
     return proxy
     
 
